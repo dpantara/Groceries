f1313acfb3d46155ead70c93e3fec8a5597bba5f
dpantara <dpantara@yahoo.com>
2018-05-21 15:16:55 +0700

    test commit 2

--- app/shared/config.js
+++ app/shared/config.js
@@ -8,7 +8,7 @@ module.exports = {
     appKey: "kid_HyHoT_REf",
     appUserHeader: "Basic a2lkX0h5SG9UX1JFZjo1MTkxMDJlZWFhMzQ0MzMyODFjN2MyODM3MGQ5OTIzMQ"
 };
-
+//test
 
 // module.exports = {
 //     apiUrl: "https://baas.kinvey.com/",
